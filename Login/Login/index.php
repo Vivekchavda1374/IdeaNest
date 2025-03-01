539053bfcd4ece49d8516e1aab45f9600a18fe2d
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-03-01 11:05:13 +0530

    login and register pages added with email id validation and database connected

--- Login/Login/index.php
+++ Login/Login/index.php
@@ -5,17 +5,6 @@
 </head>
 <body>
 <h2>Login</h2>
-<form action="../back-end/Login.php" method="POST">
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Login</button>
-</form>
 
-<h2>Register</h2>
-<form action="../back-end/register.php" method="POST">
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Register</button>
-</form>
 </body>
 </html>
