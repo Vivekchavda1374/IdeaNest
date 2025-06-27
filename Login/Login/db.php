18ee135dfdec7d5adaf8ddc3fb2f320312472448
Vivekchavda1374git config --global user.email viveksinhchavda@gmail.comgit config --global user.name Vivekchavda1374 <viveksinhchavda@gmail.com>
2025-06-27 20:27:59 +0530

    new update removed

--- Login/Login/db.php
+++ Login/Login/db.php
@@ -1,13 +1,13 @@
 <?php
-$host = "127.0.0.1";
+$host = "localhost";
 $user = "root";
-$pass = "your_password";
+$pass = "";
 $dbname = "ideanest";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection fail: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
