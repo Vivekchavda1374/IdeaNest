c529fa1bd9a523895755a2a77658e55c10c522bf
Vivekchavda1374git config --global user.email viveksinhchavda@gmail.comgit config --global user.name Vivekchavda1374 <viveksinhchavda@gmail.com>
2025-09-18 10:23:43 +0530

    all file modified done with working conditions

--- user/user_profile_setting.php
+++ user/user_profile_setting.php
@@ -297,6 +297,7 @@ $idea_count = $idea_stmt->get_result()->fetch_assoc()['count'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile Settings - IdeaNest</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/index.css">
     <link rel="stylesheet" href="../assets/css/user_profile.css">
     <style>
         .notification-toggle {
