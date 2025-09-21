732d37607210b1873adc40143c92fc76206deb27
Vivekchavda1374git config --global user.email viveksinhchavda@gmail.comgit config --global user.name Vivekchavda1374 <viveksinhchavda@gmail.com>
2025-09-22 00:34:49 +0530

    Improve comment system

--- user/comment_actions.php
+++ user/comment_actions.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 include '../Login/Login/db.php';
 
@@ -15,14 +16,14 @@ $session_id = session_id();
 switch ($action) {
     case 'like_comment':
         $comment_id = intval($_POST['comment_id']);
-        
+
         // Check if like exists
         $check_sql = "SELECT * FROM comment_likes WHERE comment_id = ? AND user_id = ?";
         $check_stmt = $conn->prepare($check_sql);
         $check_stmt->bind_param("is", $comment_id, $session_id);
         $check_stmt->execute();
         $result = $check_stmt->get_result();
-        
+
         if ($result->num_rows > 0) {
             // Remove like
             $delete_sql = "DELETE FROM comment_likes WHERE comment_id = ? AND user_id = ?";
@@ -38,18 +39,17 @@ switch ($action) {
             $insert_stmt->execute();
             $liked = true;
         }
-        
+
         // Get updated count
         $count_sql = "SELECT COUNT(*) as count FROM comment_likes WHERE comment_id = ?";
         $count_stmt = $conn->prepare($count_sql);
         $count_stmt->bind_param("i", $comment_id);
         $count_stmt->execute();
         $count = $count_stmt->get_result()->fetch_assoc()['count'];
-        
+
         echo json_encode(['success' => true, 'liked' => $liked, 'count' => $count]);
         break;
-        
+
     default:
         echo json_encode(['success' => false, 'message' => 'Invalid action']);
 }
-?>
