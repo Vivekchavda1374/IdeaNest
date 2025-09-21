6095719e00a8fa31e5dd5b3a46ad78e047f4b6e6
Vivekchavda1374git config --global user.email viveksinhchavda@gmail.comgit config --global user.name Vivekchavda1374 <viveksinhchavda@gmail.com>
2025-09-22 00:34:49 +0530

    Update CSRF protection

--- includes/csrf.php
+++ includes/csrf.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Enhanced CSRF Protection
  */
@@ -7,23 +8,27 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-function generateCSRFToken() {
+function generateCSRFToken()
+{
     if (!isset($_SESSION["csrf_token"])) {
         $_SESSION["csrf_token"] = bin2hex(random_bytes(32));
     }
     return $_SESSION["csrf_token"];
 }
 
-function validateCSRFToken($token) {
+function validateCSRFToken($token)
+{
     return isset($_SESSION["csrf_token"]) && hash_equals($_SESSION["csrf_token"], $token);
 }
 
-function getCSRFField() {
+function getCSRFField()
+{
     $token = generateCSRFToken();
     return "<input type=\"hidden\" name=\"csrf_token\" value=\"" . htmlspecialchars($token) . "\">";
 }
 
-function requireCSRF() {
+function requireCSRF()
+{
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
         if (!isset($_POST["csrf_token"]) || !validateCSRFToken($_POST["csrf_token"])) {
             http_response_code(403);
@@ -31,4 +36,3 @@ function requireCSRF() {
         }
     }
 }
-?>
